afc24f907ad4f741a2786973bacd33524e9a5797
Mark Shkreli <m.shkreli96@icloud.com>
2022-11-22 14:25:43 -0500

    commit tree 0069d0150389fb6f4812a5b2152f3a69e82a21dc

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
 
+    def name=(person_name)
+        @name = person_name
+    end
+
+    def name
+        @name
+    end
+
+    def breed=(dog_breed)
+        @breed = dog_breed
+    end
+
+    def breed
+        @breed
+    end
+
 end
